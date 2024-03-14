730e7836e0446df7663b94415ac7c7dfa3910ccc
Anvarmirzo <148099485+00016577@users.noreply.github.com>
2024-03-14 12:48:25 +0500

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const eventsRouter = require('./routes/events');
 const app = express();
-const PORT = 3000;
+const PORT = 80;
 
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
